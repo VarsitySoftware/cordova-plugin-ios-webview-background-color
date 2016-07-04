e74c9933133a6221dd625249fce4bf34fb37e496
VarsitySoftware <github@varsitysoftware.com>
2016-07-04 01:00:12 -0400

    Update WebViewBackgroundColor.js

--- www/WebViewBackgroundColor.js
+++ www/WebViewBackgroundColor.js
@@ -1,34 +1,5 @@
 var exec = require('cordova/exec');
 
-var WebViewBackgroundColor = {
-
-    isVisible: true,
-    backgroundColorByHexString: function (hexString) {
-        if (hexString.charAt(0) !== "#") {
-            hexString = "#" + hexString;
-        }
-
-        if (hexString.length === 4) {
-            var split = hexString.split("");
-            hexString = "#" + split[1] + split[1] + split[2] + split[2] + split[3] + split[3];
-        }
-
-        exec(null, null, "StatusBar", "backgroundColorByHexString", [hexString]);
-    },
-};
-
-
-// prime it. setTimeout so that proxy gets time to init
-window.setTimeout(function () {
-    exec(function (res) {
-        if (typeof res == 'object') {
-            if (res.type == 'tap') {
-                cordova.fireWindowEvent('statusTap');
-            }
-        } else {
-            WebViewBackgroundColor.isVisible = res;
-        }
-    }, null, "WebViewBackgroundColor", "_ready", []);
-}, 0);
+var WebViewBackgroundColor;
 
 module.exports = WebViewBackgroundColor;
