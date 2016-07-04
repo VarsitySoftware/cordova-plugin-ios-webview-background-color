56d7efc4b676d04fded38cded3d7289560be3ad5
VarsitySoftware <github@varsitysoftware.com>
2016-07-04 00:01:35 -0400

    Update WebViewBackgroundColor.m

--- src/ios/WebViewBackgroundColor.m
+++ src/ios/WebViewBackgroundColor.m
@@ -4,24 +4,10 @@
 
 @implementation WebViewBackgroundColor
 
-- (id)settingForKey:(NSString*)key
-{
-    return [self.commandDelegate.settings objectForKey:[key lowercaseString]];
-}
-
-- (void)observeValueForKeyPath:(NSString*)keyPath ofObject:(id)object change:(NSDictionary*)change context:(void*)context
-{
-    if ([keyPath isEqual:@"statusBarHidden"]) {
-        NSNumber* newValue = [change objectForKey:NSKeyValueChangeNewKey];
-        [self updateIsVisible:![newValue boolValue]];
-    }
-}
-
 - (void)pluginInitialize
 {
-    BOOL isiOS7 = (IsAtLeastiOSVersion(@"7.0"));
+    //BOOL isiOS7 = (IsAtLeastiOSVersion(@"7.0"));
 
-    
     self.webView.backgroundColor = [UIColor greenColor]; // Set background to a colour of your choosing
     self.webView.opaque = NO; // This makes the view transparent, you may not need this.
 
@@ -34,15 +20,6 @@
 - (void) _ready:(CDVInvokedUrlCommand*)command
 {
     _eventsCallbackId = command.callbackId;
-    [self updateIsVisible:![UIApplication sharedApplication].statusBarHidden];
-    NSString* setting = @"StatusBarOverlaysWebView";
-    if ([self settingForKey:setting]) {
-        self.statusBarOverlaysWebView = [(NSNumber*)[self settingForKey:setting] boolValue];
-        if (self.statusBarOverlaysWebView) {
-            [self resizeWebView];
-        }
-    }
 }
-
 
 @end
