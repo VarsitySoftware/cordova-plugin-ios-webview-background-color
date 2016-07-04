efb4f781223f78fc23fe917a3f3d2b0f7e0b5d81
VarsitySoftware <github@varsitysoftware.com>
2016-07-04 00:59:57 -0400

    Update WebViewBackgroundColor.m

--- src/ios/WebViewBackgroundColor.m
+++ src/ios/WebViewBackgroundColor.m
@@ -1,25 +1,30 @@
 #import "WebViewBackgroundColor.h"
-#import <objc/runtime.h>
-#import <Cordova/CDVViewController.h>
 
 @implementation WebViewBackgroundColor
 
+- (id)settingForKey:(NSString*)key
+{
+    return [self.commandDelegate.settings objectForKey:[key lowercaseString]];
+}
+
 - (void)pluginInitialize
-{
-    //BOOL isiOS7 = (IsAtLeastiOSVersion(@"7.0"));
+{   
+	NSString* setting;
 
-    self.webView.backgroundColor = [UIColor greenColor]; // Set background to a colour of your choosing
-    self.webView.opaque = NO; // This makes the view transparent, you may not need this.
+    setting  = @"WebViewBackgroundColor";
 
-}
+    if ([self settingForKey:setting]) {
+        
+		unsigned int rgbValue = 0;
+		NSScanner* scanner = [NSScanner scannerWithString:[self settingForKey:setting]];
+		[scanner setScanLocation:1];
+		[scanner scanHexInt:&rgbValue];
 
-- (void)onReset {
-    _eventsCallbackId = nil;
-}
-
-- (void) _ready:(CDVInvokedUrlCommand*)command
-{
-    _eventsCallbackId = command.callbackId;
+		UIColor* backgroundColor = [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+				
+		self.webView.backgroundColor = backgroundColor; 
+		self.webView.opaque = NO; // This makes the view transparent, you may not need this.
+    }   
 }
 
 @end
