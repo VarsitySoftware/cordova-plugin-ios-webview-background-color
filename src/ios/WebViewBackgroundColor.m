737ea971e1efecb07dbe91d51d32be1c90f3df35
Varsity Software <jweaver@iemic.com>
2019-01-23 13:48:13 -0500

    Update WebViewBackgroundColor.m

--- src/ios/WebViewBackgroundColor.m
+++ src/ios/WebViewBackgroundColor.m
@@ -2,30 +2,30 @@
 
 @implementation WebViewBackgroundColor
 
-//- (id)settingForKey:(NSString*)key
-//{
-    //return [self.commandDelegate.settings objectForKey:[key lowercaseString]];
-//}
+- (id)settingForKey:(NSString*)key
+{
+    return [self.commandDelegate.settings objectForKey:[key lowercaseString]];
+}
 
-//- (void)pluginInitialize
-//{   
-//	NSString* setting;
-//
-//    setting  = @"WebViewBackgroundColor";
-//
-//    if ([self settingForKey:setting]) {
+- (void)pluginInitialize
+{   
+	NSString* setting;
+
+    setting  = @"WebViewBackgroundColor";
+
+    if ([self settingForKey:setting]) {
         
-//		unsigned int rgbValue = 0;
-//		NSScanner* scanner = [NSScanner scannerWithString:[self settingForKey:setting]];
-//		[scanner setScanLocation:1];
-//		[scanner scanHexInt:&rgbValue];
-//
-//		UIColor* backgroundColor = [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
-//				
-//		self.webView.backgroundColor = backgroundColor; 
-//		self.webView.opaque = NO; // This makes the view transparent, you may not need this.
-//    }   
-//}
+		unsigned int rgbValue = 0;
+		NSScanner* scanner = [NSScanner scannerWithString:[self settingForKey:setting]];
+		[scanner setScanLocation:1];
+		[scanner scanHexInt:&rgbValue];
+
+		UIColor* backgroundColor = [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+				
+		self.webView.backgroundColor = backgroundColor; 
+		self.webView.opaque = NO; // This makes the view transparent, you may not need this.
+    }   
+}
 
 - (void) change:(CDVInvokedUrlCommand *)command {
     
@@ -35,6 +35,17 @@
 
     	NSLog(@"ALERT: CHANGING BACKGROUND COLOR!");
 	NSLog(@"color: %@", strColor);	
+	
+	unsigned int rgbValue = 0;
+		NSScanner* scanner = [NSScanner scannerWithString:strColor];
+		[scanner setScanLocation:1];
+		[scanner scanHexInt:&rgbValue];
+
+		UIColor* backgroundColor = [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+				
+		self.webView.backgroundColor = backgroundColor; 
+		self.webView.opaque = NO; // This makes the view transparent, you may not need this.	
+	NSLog(@"ALERT: COLOR CHANGED!");
 	
 }
 
