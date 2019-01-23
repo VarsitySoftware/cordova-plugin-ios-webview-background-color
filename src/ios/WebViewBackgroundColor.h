c1b80ca292a0fe0f32293e1ab4b5d8873d536096
Varsity Software <jweaver@iemic.com>
2019-01-23 16:56:06 -0500

    Update WebViewBackgroundColor.h

--- src/ios/WebViewBackgroundColor.h
+++ src/ios/WebViewBackgroundColor.h
@@ -1,8 +1,16 @@
 #import <Cordova/CDVPlugin.h>
-//#import <Cordova/CDVInvokedUrlCommand.h>
+#import <Cordova/CDVInvokedUrlCommand.h>
 
-@interface WebViewBackgroundColor : CDVPlugin
-
--(void)change:(CDVInvokedUrlCommand *)command;
+@interface WebViewBackgroundColor : CDVPlugin{
+    @protected
+    UIColor* _statusBarBackgroundColor;
+    NSString* _eventsCallbackId;
+}
 
 @end
+  
+//@interface WebViewBackgroundColor : CDVPlugin
+
+//-(void)change:(CDVInvokedUrlCommand *)command;
+
+
