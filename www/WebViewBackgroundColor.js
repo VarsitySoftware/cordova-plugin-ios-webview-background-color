b3c6269c357b834bb932d1356f3ef75cf391a92f
Varsity Software <jweaver@iemic.com>
2019-01-23 20:25:29 -0500

    Update WebViewBackgroundColor.js

--- www/WebViewBackgroundColor.js
+++ www/WebViewBackgroundColor.js
@@ -1,20 +1,20 @@
-var exec = require('cordova/exec');
+//var exec = require('cordova/exec');
 
-var WebViewBackgroundColor;
-module.exports = WebViewBackgroundColor;
+//var WebViewBackgroundColor;
+//module.exports = WebViewBackgroundColor;
 
-//var WebViewBackgroundColor = function () {
-//};
+var WebViewBackgroundColor = function () {
+};
 
-//WebViewBackgroundColor.prototype.change = function (success, fail, options) {
-  //if (!options) {
-    //    options = {};
-   //}
+WebViewBackgroundColor.prototype.change = function (success, fail, options) {
+  if (!options) {
+        options = {};
+   }
 
-    //return cordova.exec(success, fail, "WebViewBackgroundColor", "change", [options]);
-//};
+   return cordova.exec(success, fail, "WebViewBackgroundColor", "change", [options]);
+};
 
-//window.webViewBackgroundColor = new WebViewBackgroundColor();
+window.webViewBackgroundColor = new WebViewBackgroundColor();
 
 //var webViewBackgroundColor = window.webViewBackgroundColor;
 
