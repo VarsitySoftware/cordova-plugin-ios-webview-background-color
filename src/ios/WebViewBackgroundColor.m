d60578d55c182a16e8edf915bd7ec1843974f243
Varsity Software <jweaver@iemic.com>
2019-01-23 20:02:53 -0500

    Update WebViewBackgroundColor.m

--- src/ios/WebViewBackgroundColor.m
+++ src/ios/WebViewBackgroundColor.m
@@ -25,6 +25,7 @@
 		UIColor* backgroundColor = [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
 				
 		self.webView.backgroundColor = backgroundColor; 
+	        self.viewController.view.backgroundColor = backgroundColor;
 		self.webView.opaque = NO; // This makes the view transparent, you may not need this.
     }   
 }
